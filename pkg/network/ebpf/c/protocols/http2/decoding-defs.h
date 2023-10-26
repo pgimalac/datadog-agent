23552d7dcf3c5edd71d9abe35429bd0ae4c3a20e
amit.slavin <amit.slavin@datadoghq.com>
2023-10-26 11:10:51 +0300

    increased interesting frames

--- pkg/network/ebpf/c/protocols/http2/decoding-defs.h
+++ pkg/network/ebpf/c/protocols/http2/decoding-defs.h
@@ -7,8 +7,8 @@
 
 // Maximum number of frames to be processed in a single TCP packet. That's also the number of tail calls we'll have.
 // NOTE: we may need to revisit this const if we need to capture more connections.
-#define HTTP2_MAX_FRAMES_ITERATIONS 10
-#define HTTP2_MAX_FRAMES_TO_FILTER  100
+#define HTTP2_MAX_FRAMES_ITERATIONS 20
+#define HTTP2_MAX_FRAMES_TO_FILTER  40
 
 // A limit of max headers which we process in the request/response.
 #define HTTP2_MAX_HEADERS_COUNT_FOR_FILTERING 20
