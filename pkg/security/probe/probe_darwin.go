70ab3e38189b5b1296b0e9ed8cfb3581c94a815c
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-11-30 17:33:41 +0100

    empty Darwin probe

--- pkg/security/probe/probe_darwin.go
+++ pkg/security/probe/probe_darwin.go
@@ -8,7 +8,42 @@ package probe
 
 import (
 	"github.com/DataDog/datadog-agent/pkg/security/config"
+	"github.com/DataDog/datadog-agent/pkg/security/probe/kfilters"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/compiler/eval"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
+	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
 )
+
+type DarwinProbe struct {
+}
+
+func NewDarwinProbe(p *Probe, config *config.Config, opts Opts) (*DarwinProbe, error) {
+	return &DarwinProbe{}, nil
+}
+
+func (dp *DarwinProbe) Setup() error                    { return nil }
+func (dp *DarwinProbe) Init() error                     { return nil }
+func (dp *DarwinProbe) Start() error                    { return nil }
+func (dp *DarwinProbe) Stop()                           {}
+func (dp *DarwinProbe) SendStats() error                { return nil }
+func (dp *DarwinProbe) Snapshot() error                 { return nil }
+func (dp *DarwinProbe) Close() error                    { return nil }
+func (dp *DarwinProbe) NewModel() *model.Model          { return nil }
+func (dp *DarwinProbe) DumpDiscarders() (string, error) { return "", nil }
+func (dp *DarwinProbe) FlushDiscarders() error          { return nil }
+func (dp *DarwinProbe) ApplyRuleSet(_ *rules.RuleSet) (*kfilters.ApplyRuleSetReport, error) {
+	return nil, nil
+}
+func (dp *DarwinProbe) OnNewDiscarder(_ *rules.RuleSet, _ *model.Event, _ eval.Field, _ eval.EventType) {
+}
+func (dp *DarwinProbe) HandleActions(_ *rules.Rule, _ eval.Event) {}
+func (dp *DarwinProbe) NewEvent() *model.Event {
+	return nil
+}
+func (dp *DarwinProbe) GetFieldHandlers() model.FieldHandlers                { return nil }
+func (dp *DarwinProbe) DumpProcessCache(_ bool) (string, error)              { return "", nil }
+func (dp *DarwinProbe) AddDiscarderPushedCallback(_ DiscarderPushedCallback) {}
+func (dp *DarwinProbe) GetEventTags(_ string) []string                       { return nil }
 
 // NewProbe instantiates a new runtime security agent probe
 func NewProbe(config *config.Config, opts Opts) (*Probe, error) {
@@ -21,18 +56,16 @@ func NewProbe(config *config.Config, opts Opts) (*Probe, error) {
 		scrubber:     newProcScrubber(config.Probe.CustomSensitiveWords),
 	}
 
-	/*
-		pp, err := NewDarwinProbe(p, config, opts)
-		if err != nil {
-			return nil, err
-		}
-		p.PlatformProbe = pp
+	pp, err := NewDarwinProbe(p, config, opts)
+	if err != nil {
+		return nil, err
+	}
+	p.PlatformProbe = pp
 
-		p.event = p.PlatformProbe.NewEvent()
+	p.event = p.PlatformProbe.NewEvent()
 
-		// be sure to zero the probe event before everything else
-		p.zeroEvent()
-	*/
+	// be sure to zero the probe event before everything else
+	p.zeroEvent()
 
 	return p, nil
 }
