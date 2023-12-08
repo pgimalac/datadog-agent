b2663a33701f9cb156b381bc9bc7a037a349f073
Guy Arbitman <guy20495@gmail.com>
2023-12-08 14:57:59 +0200

    usm: http2: Add user mode consumer to the perf buffer
    The dynamic table management is now consuming the paths sent from the kernel.
    We use an LRU datastore to automatically evict the least recently used element
    when the map is full. On eviction we're deleting the matching entry from
    http2_interesting_dynamic_table_set map.
    
    The LRU value is a string internning pointer, to ensure we're using
    a single instance and memory for the same path, even if the key is
    different.

--- pkg/network/protocols/http2/protocol.go
+++ pkg/network/protocols/http2/protocol.go
@@ -49,18 +49,19 @@ type Protocol struct {
 }
 
 const (
-	inFlightMap                = "http2_in_flight"
-	dynamicTable               = "http2_dynamic_table"
-	interestingDynamicTableSet = "http2_interesting_dynamic_table_set"
-	dynamicTableCounter        = "http2_dynamic_counter_table"
-	http2IterationsTable       = "http2_iterations"
-	staticTable                = "http2_static_table"
-	firstFrameHandlerTailCall  = "socket__http2_handle_first_frame"
-	filterTailCall             = "socket__http2_filter"
-	headersParserTailCall      = "socket__http2_headers_parser"
-	eosParserTailCall          = "socket__http2_eos_parser"
-	eventStream                = "http2"
-	telemetryMap               = "http2_telemetry"
+	inFlightMap                 = "http2_in_flight"
+	dynamicTable                = "http2_dynamic_table"
+	interestingDynamicTableSet  = "http2_interesting_dynamic_table_set"
+	dynamicTableCounter         = "http2_dynamic_counter_table"
+	http2IterationsTable        = "http2_iterations"
+	staticTable                 = "http2_static_table"
+	firstFrameHandlerTailCall   = "socket__http2_handle_first_frame"
+	filterTailCall              = "socket__http2_filter"
+	headersParserTailCall       = "socket__http2_headers_parser"
+	eosParserTailCall           = "socket__http2_eos_parser"
+	eventStream                 = "http2"
+	telemetryMap                = "http2_telemetry"
+	http2DynamicTablePerfBuffer = "http2_dynamic_table_perf_buffer"
 )
 
 // Spec is the protocol spec for HTTP/2.
@@ -147,7 +148,7 @@ func newHTTP2Protocol(cfg *config.Config) (protocols.Protocol, error) {
 		telemetry:                  telemetry,
 		http2Telemetry:             http2KernelTelemetry,
 		kernelTelemetryStopChannel: make(chan struct{}),
-		dynamicTable:               NewDynamicTable(),
+		dynamicTable:               NewDynamicTable(int(cfg.MaxUSMConcurrentRequests)),
 	}, nil
 }
 
